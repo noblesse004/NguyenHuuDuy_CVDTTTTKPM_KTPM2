8fddc0f35ecda374aa112babbbff57f4d52f40bc
Nguyễn Hữu Duy <123649327+noblesse004@users.noreply.github.com>
2025-04-26 07:36:37 +0700

    Delete ThanhToanTienMat.java

--- ThanhToanTienMat.java
+++
@@ -1,8 +0,0 @@
-package ThanhToan;
-
-public class ThanhToanTienMat implements IThanhToan {
-    @Override
-    public void ThanhToan(double soTien) {
-        System.out.println("Thanh toán bằng tiền mặt: " + soTien + " VND");
-    }
-}
