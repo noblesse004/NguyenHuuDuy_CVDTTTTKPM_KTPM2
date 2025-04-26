9826807fa165f521c23b951cb8e2db8c4cc1f586
Nguyễn Hữu Duy <123649327+noblesse004@users.noreply.github.com>
2025-04-26 07:36:18 +0700

    Delete ThanhToanTheTinDung.java

--- ThanhToanTheTinDung.java
+++
@@ -1,8 +0,0 @@
-package ThanhToan;
-
-public class ThanhToanTheTinDung implements IThanhToan {
-    @Override
-    public void ThanhToan(double soTien) {
-        System.out.println("Thanh toán bằng thẻ tín dụng: " + soTien + " VND");
-    }
-}
